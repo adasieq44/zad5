3409333712b5932284f698254b1f334aaca0e7e3
Mateusz <matikonkursiakpl@gmail.com>
2026-03-23 10:45:48 +0100

    Aktualizacja testow

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -11,6 +11,10 @@ def test_load_data():
     assert isinstance(manager.transfers, list)
     assert isinstance(manager.bills, list)
 
+    lista = ["Jan Nowak", "Adam Kowalski", "Ewa Adamska"]
     for apartment_key, apartment in manager.apartments.items():
         assert isinstance(apartment, Apartment)
         assert apartment.key == apartment_key
+        for tenant_key, tenant in manager.tenants.items():
+            assert tenant.name in lista
+        
--- tests/test_unit.py
+++ tests/test_unit.py
@@ -2,6 +2,7 @@ import pytest
 
 from pydantic import ValidationError
 from src.models import Apartment
+from src.manager import Tenant
 
 
 def test_apartment_fields():
@@ -10,7 +11,6 @@ def test_apartment_fields():
         name="Test Apartment",
         location="Test Location",
         area_m2=50.0,
-        Tenant=6,
         rooms={
             "room-1": {"name": "Living Room", "area_m2": 30.0},
             "room-2": {"name": "Bedroom", "area_m2": 20.0}
@@ -21,7 +21,24 @@ def test_apartment_fields():
     assert data.location == "Test Location"
     assert data.area_m2 == 50.0
     assert len(data.rooms) == 2
-    assert data.Tenant == 6
+    
+def test_tenant_fields():
+    data = Tenant(
+        apartment="tenant-test",
+        name="Test Tenant",
+        room="Test Room",
+        rent_pln=1400.0,
+        deposit_pln = 3000.0,
+        date_agreement_from="2024-01-01",
+        date_agreement_to="2024-12-31"
+    )
+    assert data.apartment == "tenant-test"
+    assert data.name == "Test Tenant"
+    assert data.room == "Test Room"
+    assert data.rent_pln == 1400.0
+    assert data.deposit_pln == 3000.0
+    assert data.date_agreement_from == "2024-01-01"
+    assert data.date_agreement_to == "2024-12-31"
     
 
 
@@ -31,7 +48,6 @@ def test_apartment_from_dict():
         "name": "Test Apartment",
         "location": "Test Location",
         "area_m2": 50.0,
-        "Tenant": 4,
         "rooms": {
             "room-1": {"name": "Living Room", "area_m2": 30.0},
             "room-2": {"name": "Bedroom", "area_m2": 20.0}
@@ -43,8 +59,21 @@ def test_apartment_from_dict():
     assert apartment.location == data["location"]
     assert apartment.area_m2 == data["area_m2"]
     assert len(apartment.rooms) == len(data["rooms"])
-    assert apartment.Tenant == 4
 
     data['area_m2'] = "25m2" # Invalid field
     with pytest.raises(ValidationError):
         wrong_apartment = Apartment(**data)
+
+def test_tenant_from_dict():
+    data = {
+        "name": "Jan Nowak",
+        "apartment": "apart-polanka",
+        "room": "room-bigger",
+        "deposit_pln": 3000.0,
+    }
+    tenant = Tenant(**data)
+    assert tenant.name == data["name"]
+    assert tenant.apartment == data["apartment"]
+    assert tenant.room == data["room"]
+    assert tenant.deposit_pln == data["rent_pln"]
+    
